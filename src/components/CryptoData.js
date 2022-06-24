c1af88bea55aa01798ac6db119a3cfb7293e16fd
Ralph26 <ralph90062@gmail.com>
2022-06-24 00:11:41 -0700

    "apichallenge"

--- src/components/CryptoData.js
+++ src/components/CryptoData.js
@@ -1,16 +1,31 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 
+// function something(){
+//     continue
+// }
+
 const CryptoData = () => {
     // data: represents our data, setdata sets the data we are pulling from the api
-    const [data, setData] = useState([]);
-    // collect user data, empty string
-    const baseUrl = "https://www.cryptingup.com/api";
+    const [data1, setData1] = useState([]);
+    const [data2, setData2] = useState([]);
+    const [data3, setData3] = useState([]);
     
     // Make a request to all exchanges
     useEffect(() => {
-        axios.get(`${baseUrl}/exchanges`).then(res => {
-            setData(res.data.exchanges);
+        axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+            console.log (res)
+            setData1(res.data.markets);
+        });
+
+        axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+            setData2(res.data.markets);
+            
+        });
+
+        axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+            setData3(res.data.markets);
+        
         });
     }, []);
    
@@ -18,11 +33,15 @@ const CryptoData = () => {
     return (
         <div>
             <h1>Data from my API</h1>
-            {data.map(x => <li key={x.exchange_id}>{x.name}</li>)}
+        
             
             {/* List specific data for the 3 exchanges  */}
             <h1>Single Post data</h1>
-            <h3>Exchange Name: </h3>
+
+<h3>Price:  {data3.map((x, i) => <li key={i}>{x.price}</li>)} </h3>
+            <h3>Symbol: {data1.map((x, i) => <li key={i}>{x.symbol}</li>)} </h3>
+            <h3>Exchange Name: {data2.map((x, i) => <li key={i}>{x.exchange_id}</li>)} </h3>
+            
             <p> </p>
         </div>  
     )
